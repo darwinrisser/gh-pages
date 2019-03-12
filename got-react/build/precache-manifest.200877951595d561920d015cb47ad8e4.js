6701a1514d17f60c16b2a8f75a29e61a4e4a1431
Darwin Risser <riser.darwin@gmail.com>
2019-03-11 21:38:45 -0500

    added first app

---
+++ got-react/build/precache-manifest.200877951595d561920d015cb47ad8e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "714b446fe08f8ca50dce",
+    "url": "/static/js/main.714b446f.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "714b446fe08f8ca50dce",
+    "url": "/static/css/main.c4b6e597.chunk.css"
+  },
+  {
+    "revision": "9dc4f63d640f085ba37a8dc5aaf9b367",
+    "url": "/index.html"
+  }
+];
