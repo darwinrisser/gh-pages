eaa6cd795a255c1974c387c35bfab0308472c360
Darwin Risser <riser.darwin@gmail.com>
2019-03-12 10:16:05 -0500

    new rrr

---
+++ river-rat-raku/precache-manifest.2bf29010cc712eb86d2eb3d740e6230d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fced654be02049f1bb6c",
+    "url": "/gh-pages/river-rat-raku/static/js/runtime~main.fced654b.js"
+  },
+  {
+    "revision": "b534b0925ddc987f39e2",
+    "url": "/gh-pages/river-rat-raku/static/js/main.b534b092.chunk.js"
+  },
+  {
+    "revision": "bbb919570400360e0620",
+    "url": "/gh-pages/river-rat-raku/static/js/1.bbb91957.chunk.js"
+  },
+  {
+    "revision": "b534b0925ddc987f39e2",
+    "url": "/gh-pages/river-rat-raku/static/css/main.6be85422.chunk.css"
+  },
+  {
+    "revision": "35341b8badbc19ac83db6e3f1d42be90",
+    "url": "/gh-pages/river-rat-raku/index.html"
+  }
+];
---
+++ river-rat-raku/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/gh-pages/river-rat-raku/precache-manifest.2bf29010cc712eb86d2eb3d740e6230d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/gh-pages/river-rat-raku/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
