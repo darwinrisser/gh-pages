d5e64d305ca9bbe79ffbe83bf9fbec5c53c73355
Darwin Risser <riser.darwin@gmail.com>
2019-03-11 22:07:44 -0500

    added rrr

---
+++ river-rat-raku/precache-manifest.dfb9b402280306bdf8a1d15e38fa9db5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fced654be02049f1bb6c",
+    "url": "/gh-pages/river-rat-raku/static/js/runtime~main.fced654b.js"
+  },
+  {
+    "revision": "b844ce0410e587a73c1b",
+    "url": "/gh-pages/river-rat-raku/static/js/main.b844ce04.chunk.js"
+  },
+  {
+    "revision": "bbb919570400360e0620",
+    "url": "/gh-pages/river-rat-raku/static/js/1.bbb91957.chunk.js"
+  },
+  {
+    "revision": "b844ce0410e587a73c1b",
+    "url": "/gh-pages/river-rat-raku/static/css/main.bfad759a.chunk.css"
+  },
+  {
+    "revision": "4a63936b89ebb1dd31190c2719b5eeb4",
+    "url": "/gh-pages/river-rat-raku/index.html"
+  }
+];
