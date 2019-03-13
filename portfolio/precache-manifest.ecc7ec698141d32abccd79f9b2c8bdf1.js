a02af0eaaf6b4df1a11a69e1739aaecb377c5263
Darwin Risser <riser.darwin@gmail.com>
2019-03-12 21:19:55 -0500

    fixed url

---
+++ portfolio/precache-manifest.ecc7ec698141d32abccd79f9b2c8bdf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "23afcefb28a9a2aef368",
+    "url": "/portfolio/static/js/main.23afcefb.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/portfolio/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "23afcefb28a9a2aef368",
+    "url": "/portfolio/static/css/main.c509c95b.chunk.css"
+  },
+  {
+    "revision": "cc899ff596361f759c098cdb7bd1dc0c",
+    "url": "/portfolio/index.html"
+  }
+];
--- portfolio/service-worker.js
+++ portfolio/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d33a026e84b8700b388c28a7ba680ae.js"
+  "/portfolio/precache-manifest.ecc7ec698141d32abccd79f9b2c8bdf1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
