0f4dae2d7d04e7e7fb5c6860ed318c3907f09a93
Darwin Risser <riser.darwin@gmail.com>
2019-03-12 21:15:36 -0500

    added portfolio

---
+++ portfolio/precache-manifest.4d33a026e84b8700b388c28a7ba680ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8c9ae339e292150ccfd2",
+    "url": "/static/js/main.8c9ae339.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "8c9ae339e292150ccfd2",
+    "url": "/static/css/main.c509c95b.chunk.css"
+  },
+  {
+    "revision": "5b06ccff9c9d73d84c77bc0ca14fb994",
+    "url": "/index.html"
+  }
+];
